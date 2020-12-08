e16dc6e310245cb79ac2f78dcb17624781c25cc9
Alison <alisongarantizado@gmail.com>
2020-12-08 13:59:09 -0400

    Boilerplate with TypeScript

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+        <div>
+          <title>React Avancado</title>
+          <h1>React Avancado</h1>
+        </div>
+  )
+}
